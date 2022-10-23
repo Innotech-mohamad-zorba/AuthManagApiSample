7480c2aecd6ced7e859c8295a47171b5b28c86e3
Mohamad Zorba <innotech-mohammad-zorba>
2022-10-23 11:22:13 +0300

    upload the sample code

---
+++ Default.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Web.UI;
+using Auth0.ManagementApi;
+using Auth0.ManagementApi.Models;
+
+namespace managementApi
+{
+    public partial class _Default : Page
+    {
+        private readonly string _accessToken = "put correct value from your side";
+        private readonly string _email = "put correct value from your side";
+        private readonly string _auth0Domain = "put correct value from your side";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                var manApiClient = new ManagementApiClient(_accessToken, new Uri(string.Format("https://{0}/api/v2", _auth0Domain)));
+
+                // pending here with no response or exceptions, some strange behavior
+                var users = manApiClient.Users.GetAllAsync(new GetUsersRequest() { Query = $@"email:""{_email.ToLower()}""", SearchEngine = "v3" }).GetAwaiter().GetResult();
+            }
+            catch (Exception)
+            {
+            }
+        }
+    }
+}
